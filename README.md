856924d5156f536ce7084a2a5fd723ad857bd271
Zero isnt Cool <69110758+zeroisntcool@users.noreply.github.com>
2020-08-25 08:39:25 +0300

    Twitter bot

---
+++ README.md
@@ -0,0 +1,45 @@
+# ProxyTwitter
+
+<img src="images/bash_pic.png">
+<img src="images/twitter_pic.png">
+
+## How it works
+
+* Fetches new proxy addresses many sites and then tweets them to twitter.
+* Program is made with python3
+
+## Requirments
+
+```python
+import tweepy
+import time
+import asyncio
+from proxybroker import Broker
+
+install them simply pip3 install -r requirments
+```
+
+## Tokens
+
+```python
+api = ""
+api_secret = ""
+access_token = ""
+access_token_secret = ""
+```
+
+Get tokens from <https://developer.twitter.com> and start tweeting.
+If you want edit script like what proxies it will find, what countries etc..
+<https://proxybroker.readthedocs.io/en/latest/> that will help you.
+
+## Running
+
+Install all requirments and run it with python3.  
+
+```nohup python3 main.py > output.log  &```
+   
+nohup will run this script at background.  
+If you wanna stop the process running do those next commands:
+
+```ps | grep main.py``` #Find PID number  
+```kill PID``` #Kill the process with PID number
---
+++ main.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import tweepy
+import time
+import asyncio
+from proxybroker import Broker
+
+
+# Authenticate to Twitter (developer.twitter.com)
+api = "h62iSPak7kJDwO8ID6WHjYsWU"
+api_secret = "lq40KvwXfctc5bjtWvl5uxGT01jvIp77pHZrNzrej39sW6gZGy"
+access_token = "937334422118027265-9GfliYPtDhp1qeYQTaSzcUqbHRW2J5F"
+access_token_secret = "zSb3GaWrKrq5BLTjgxsfKlMzMV889wnBNEnF5ysCWKNLT"
+auth = tweepy.OAuthHandler(api, api_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+def verify():
+	try:
+		api.verify_credentials()
+		print("\n", "	\033[36mTwitter\033[39m bot started ")
+		print("	tweeting four tweets per hour ;) ", "\n")
+	except:
+		print("	Error, no faking idea check your tokens")
+verify()
+async def tweet(proxies):
+    while True:
+      try:
+        proxy = await proxies.get()
+        if proxy is None: break
+        api.update_status(proxy)
+        print(proxy)
+        time.sleep(900)
+      except tweepy.TweepError as e:
+        print("\033[36m" + str(e.api_code) + "\033[39m This address is already tweeted.")
+      except KeyboardInterrupt:
+        print("exit")
+proxies = asyncio.Queue()
+broker = Broker(proxies)
+tasks = asyncio.gather(
+    broker.find(types=["HTTP", "HTTPS", "Anonymous", "High"], limit=9999, timeout=8, attempts_tries=3, providers=['http://www.proxylists.net/', 'http://fineproxy.org/eng/', 'https://www.proxynova.com/proxy-server-list/', 'http://www.freeproxylists.net/', 'https://hidemy.name/en/proxy-list/', 'http://free-proxy.cz/en/'], countries = ['US', 'CN', 'BR', 'RU']),
+		tweet(proxies))
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(tasks)	
+
+
